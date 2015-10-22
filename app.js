b14363f7b3653c7250eba8865520c6a6b3e67965
Mattias Ewers <mattias.ewers@gmail.com>
2015-10-22 16:14:40 +0200

    Blog and Home update

--- app.js
+++ app.js
@@ -103,7 +103,7 @@ app.set('view engine', 'ejs');
 
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(paginate.middleware(3, 50));
+app.use(paginate.middleware(2, 50));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp');
 var autoprefixer = require('autoprefixer');
 var minifyCss = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
-
+var eslint = require('gulp-eslint');
 
 gulp.task('css', function () {
     var processors = [
@@ -18,14 +18,24 @@ gulp.task('css', function () {
         .pipe(gulp.dest('./stylesheets/dist'));
 });
 
-
 gulp.task('minify-css', function() {
   return gulp.src('./stylesheets/dist/styles.css')
     .pipe(minifyCss({compatibility: 'ie8'}))
     .pipe(gulp.dest('./public/css/'));
 });
 
-
+gulp.task('lint', function () {
+    return gulp.src(['./public/js/*.js'])
+        // eslint() attaches the lint output to the eslint property
+        // of the file object so it can be used by other modules.
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console.
+        // Alternatively use eslint.formatEach() (see Docs).
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on
+        // lint error, return the stream and pipe to failAfterError last.
+        .pipe(eslint.failAfterError());
+});
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
